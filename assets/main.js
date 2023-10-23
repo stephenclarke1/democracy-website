f908862f751afead8b7100f0b897faca27959e8d
stephenclarke1 <78845346+stephenclarke1@users.noreply.github.com>
2023-10-24 12:40:38 +1300

    started module menu and now need to redo mobile menu so that its flexible

--- assets/main.js
+++ assets/main.js
@@ -36,7 +36,25 @@ if (formData === null) {
         };
     };
 };
-}
+};
+
+// toggle 'dropdown-visible' class for dropdown content
+function myFunction(a) {
+    a.parentNode.getElementsByClassName("dropdown-submenu")[0].classList.toggle("dropdown-visible");
+};
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-button')) { 
+      var dropdowns = document.getElementsByClassName("dropdown-submenu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('dropdown-visible')) {
+          openDropdown.classList.remove('dropdown-visible');
+        }
+      }
+    }
+  };
 
 function menuToggle () {
     const menuButton = document.getElementById("menuButton");
